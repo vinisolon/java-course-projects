f0e49cd45746f5bd6eb7ef7ae81366a9c2adcc2e
Vinicius Solon <vini.97@live.com>
2020-11-11 20:47:35 -0300

    teste de comando de auto identação da IDE IntelliJ

--- file-reader&buffered-reader/src/application/Program.java
+++ file-reader&buffered-reader/src/application/Program.java
@@ -7,19 +7,19 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		//Metodo basico de uso dos readers
-		
+
 		String path = "C:\\Users\\Vini\\Desktop\\Dev\\Java\\file-reader&buffered-reader\\text.txt";
 		FileReader fr = null;
 		BufferedReader br = null;
-		
+
 		try {
 			fr = new FileReader(path);
 			br = new BufferedReader(fr);
-			
+
 			String line = br.readLine();
-			
+
 			while(line != null) {
 				System.out.println(line);
 				line = br.readLine();
@@ -41,7 +41,7 @@ public class Program {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 }
