40fbd28d1a687163ca2c8b884977723a671352f1
Vinicius Solon <vini.97@live.com>
2020-11-12 22:24:46 -0300

    Melhoria da lógica acerca do calculo de pagamento

--- interfaces2/src/servicos/ServicoAluguel.java
+++ interfaces2/src/servicos/ServicoAluguel.java
@@ -21,14 +21,22 @@ public class ServicoAluguel {
 
     public void processoDeFatura(Aluguel aluguel){
         long diferencaMS = aluguel.getFim().getTime() - aluguel.getInicio().getTime(); // MS
-        double duracaoHoras = diferencaMS / 1000 / 60 / 60; // MS / SEG / HORA
-        double pagamentoBasico = 0.0;
+        int duracaoHoras = (int) diferencaMS / 3600000; // 3600000 = 60 * 60 * 1000
+        int duracaoMinutos = (int) (diferencaMS / 60000) % 60;
+
+        System.out.println(String.format("\nTempo total em horas de aluguel: %03d:%02d", duracaoHoras, duracaoMinutos));
+
+        double pagamentoBasico;
+
+        if(duracaoMinutos > 30) duracaoHoras += 1;
 
         if(duracaoHoras <= 12){
-            pagamentoBasico = Math.ceil(duracaoHoras) * precoHora;
+            pagamentoBasico = duracaoHoras * precoHora;
         }
         else{
-            pagamentoBasico = Math.ceil(duracaoHoras / 24) * precoDia;
+            int dias = (int) Math.floor(duracaoHoras / 24); // EX: 77 / 24 = 3,2 <---> floor(77 / 24) = 3
+            int difHoras =  duracaoHoras - (dias * 24) ;
+            pagamentoBasico = (dias * precoDia) + (difHoras * precoHora);
         }
 
         double taxa = servicoTaxa.taxa(pagamentoBasico);
