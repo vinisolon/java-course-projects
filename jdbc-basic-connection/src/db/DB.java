2c1cce4d5462894fd9b7b9576476864672d25865
Vinicius Solon <vini.97@live.com>
2020-12-06 13:05:55 -0300

    Adicionados métodos para fechamento de statement e resultset

--- jdbc-basic-connection/src/db/DB.java
+++ jdbc-basic-connection/src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -37,6 +35,25 @@ public class DB {
         if(connection != null){
             try {
                 connection.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+    public static void closeStatement(Statement stm) {
+        if(stm != null) {
+            try {
+                stm.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if(rs != null) {
+            try {
+                rs.close();
             } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
